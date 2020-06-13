ec3f492e96104b742d3257dd5ee811274e9b90e1
lisknonanika <lisknonanika@gmail.com>
2020-06-13 23:28:13 +0900

    upd

--- transactions/pwd_lock_send_transaction.js
+++ transactions/pwd_lock_send_transaction.js
@@ -1,4 +1,5 @@
-const { BaseTransaction, TransactionError, utils } = require("@liskhq/lisk-transactions");
+const { BaseTransaction, TransactionError, utils, constants } = require("@liskhq/lisk-transactions");
+const BigNum = require("@liskhq/bignum");
 const Ajv = require("ajv");
 const sendSchema = require("./json_schema/send_asset_schema");
 const trxConfig = require("./config");
@@ -65,7 +66,7 @@ class PwdLockSendTransaction extends BaseTransaction {
 
             if (this.asset.data.amount) {
                 // amount = data.amount ?
-                if (+this.asset.data.amount !== +utils.convertLSKToBeddows(this.amount)) {
+                if (+this.asset.data.amount !== +utils.convertBeddowsToLSK(this.amount.toString())) {
                     errors.push(new TransactionError("should match 'amount'", this.id, ".data.amount"));
                 }
     
@@ -76,15 +77,39 @@ class PwdLockSendTransaction extends BaseTransaction {
                 }
             }
         }
+        if (!utils.validateTransferAmount(this.amount.toString())) {
+            errors.push(new TransactionError('Amount must be a valid number in string format.', this.id, '.amount', this.amount.toString()));
+        }
+
+        if (this.recipientId) {
+            errors.push(new TransactionError('Invalid parameter', this.id, '.recipientId'));
+        }
+        
+        if (this.recipientPublicKey) {
+            errors.push(new TransactionError('Invalid parameter', this.id, '.recipientPublicKey'));
+        }
     }
 
     applyAsset(store) {
         const errors = [];
+        const sender = store.account.get(this.senderId);
+        const balanceError = utils.verifyAmountBalance(this.id, sender, this.amount, this.fee);
+        if (balanceError) {
+            errors.push(balanceError);
+        }
+        const afterBalance = new BigNum(sender.balance).sub(this.amount);
+        store.account.set(updatedSender.address, {...sender, balance: afterBalance.toString()});
         return errors;
     }
 
     undoAsset(store) {
         const errors = [];
+        const sender = store.account.get(this.senderId);
+        const afterBalance = new BigNum(sender.balance).add(this.amount);
+        if (afterBalance.gt(constants.MAX_TRANSACTION_AMOUNT)) {
+            errors.push(new TransactionError('Invalid amount', this.id, '.amount', this.amount.toString()));
+        }
+        store.account.set(updatedSender.address, {...sender, balance: afterBalance.toString()});
         return errors;
     }
 }
